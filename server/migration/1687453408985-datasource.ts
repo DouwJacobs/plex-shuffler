44eb563d76c0986564c8e77cc5be3abd83c7d469
Douw Jacobs <douw1.jacobs@gmail.com>
2023-06-22 19:06:12 +0200

    fix(migration): added SQLite Migration
    Added SQLMigration for prod tables

--- server/migration/1687453408985-datasource.ts
+++ server/migration/1687453408985-datasource.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Datasource1687451910597 implements MigrationInterface {
+export class Datasource1687453408985 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {}
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
