dc536cbac17c77715acab96781e96617efbe47fc
Douw Jacobs <douw1.jacobs@gmail.com>
2023-07-13 10:35:41 +0200

    perf(title cards): boop
    Improved transitions for card boops. Also removed particle js to increase performance

--- src/components/Common/TitleCard/index.tsx
+++ src/components/Common/TitleCard/index.tsx
@@ -4,6 +4,7 @@ import Checkbox from '@app/components/Common/Checkbox';
 import LoadingSpinner from '@app/components/Common/LoadingSpinner';
 import ErrorCard from '@app/components/Common/TitleCard/ErrorCard';
 import Placeholder from '@app/components/Common/TitleCard/Placeholder';
+import { useIsTouch } from '@app/hooks/useIsTouch';
 import { withProperties } from '@app/utils/typeHelpers';
 import { useState } from 'react';
 
@@ -17,39 +18,41 @@ type PlaylistCardProps = {
 };
 
 const TitleCard = (props: PlaylistCardProps) => {
+  const isTouch = useIsTouch();
   const [isLoading, setIsLoading] = useState(true);
+  const [boop, setBoop] = useState(false);
 
   return (
     <Card
-      className="h-full transform text-center text-white transition duration-100
-    hover:scale-110 sm:w-36 md:w-44"
+      className={`h-full cursor-default text-center text-white transition duration-300 sm:w-36 md:w-44 ${
+        boop ? 'scale-105 shadow-lg' : 'scale-100 shadow'
+      }`}
     >
-      <div className="m-2 pt-2">
-        {isLoading && (
-          <div>
-            <LoadingSpinner className="h-full" />
-          </div>
-        )}
+      <div
+        onMouseEnter={() => {
+          if (!isTouch) {
+            setBoop(true);
+          }
+        }}
+        onMouseLeave={() => setBoop(false)}
+      >
+        <div className="m-2 pt-2">
+          {isLoading && (
+            <div>
+              <LoadingSpinner className="h-full" />
+            </div>
+          )}
 
-        <div className={`checkbox-card ${isLoading && 'hidden'}`}>
-          {props.handleOnchange ? (
-            <Checkbox
-              selected={props.selected}
-              handleOnchange={props.handleOnchange}
-              id={props.ratingKey}
-            />
-          ) : null}
-          <label htmlFor={props.ratingKey}>
+          <div className={`checkbox-card ${isLoading && 'hidden'}`}>
             {props.handleOnchange ? (
-              <CachedImage
-                alt={props.title}
-                src={props.thumb}
-                onLoad={() => {
-                  setIsLoading(false);
-                }}
+              <Checkbox
+                selected={props.selected}
+                handleOnchange={props.handleOnchange}
+                id={props.ratingKey}
               />
-            ) : (
-              <a href={props.url} target="_blank" rel="noreferrer">
+            ) : null}
+            <label htmlFor={props.ratingKey}>
+              {props.handleOnchange ? (
                 <CachedImage
                   alt={props.title}
                   src={props.thumb}
@@ -57,23 +60,33 @@ const TitleCard = (props: PlaylistCardProps) => {
                     setIsLoading(false);
                   }}
                 />
-              </a>
-            )}
-          </label>
+              ) : (
+                <a href={props.url} target="_blank" rel="noreferrer">
+                  <CachedImage
+                    alt={props.title}
+                    src={props.thumb}
+                    onLoad={() => {
+                      setIsLoading(false);
+                    }}
+                  />
+                </a>
+              )}
+            </label>
+          </div>
         </div>
+        <h1
+          className="text-l whitespace-normal font-bold leading-tight text-white"
+          style={{
+            WebkitLineClamp: 3,
+            display: '-webkit-box',
+            overflow: 'hidden',
+            WebkitBoxOrient: 'vertical',
+            wordBreak: 'break-word',
+          }}
+        >
+          {props.title}
+        </h1>
       </div>
-      <h1
-        className="text-l whitespace-normal font-bold leading-tight text-white"
-        style={{
-          WebkitLineClamp: 3,
-          display: '-webkit-box',
-          overflow: 'hidden',
-          WebkitBoxOrient: 'vertical',
-          wordBreak: 'break-word',
-        }}
-      >
-        {props.title}
-      </h1>
     </Card>
   );
 };
